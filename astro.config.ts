7cbd6f61ec13351c2f9da2dd09e58e173e3bc036
wcapitalcodes <uzzair@wcapital.asia>
2024-12-13 22:07:31 +0800

    added

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
 import type { AstroIntegration } from 'astro';
+import svelte from '@astrojs/svelte';
 
 import astrowind from './vendor/integration';
 
@@ -30,6 +31,7 @@ export default defineConfig({
     }),
     sitemap(),
     mdx(),
+    svelte(),
     icon({
       include: {
         tabler: ['*'],
---
+++ src/components/NetlifyIdentity.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onMount} from 'svelte'
+    onMount(() => {
+        if (window.netlifyIdentity) {
+            window.netlifyIdentity.on('init', (user) => {
+                if (!user) {
+                    window.netlifyIdentity.on('login', () => {
+                        document.location.href = '/admin/';
+                    });
+                }
+            });
+        }
+    })
+</script>
