a20dd838e7332700a65d964c0ef0628e0671e14f
wcapitalcodes <uzzair@wcapital.asia>
2024-12-25 14:02:54 +0800

    setup TeamCard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,45 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import plugin from 'tailwindcss/plugin';
 import typographyPlugin from '@tailwindcss/typography';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--aw-color-primary)',
+        primary: colors.teal,
         secondary: 'var(--aw-color-secondary)',
-        accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        'accent': {
+          DEFAULT: '#8E1246',
+          50: '#FFFCFD',
+          100: '#FCEAF2',
+          200: '#F8C6DB',
+          300: '#F3A2C4',
+          400: '#EF7DAD',
+          500: '#EA5996',
+          600: '#E5357F',
+          700: '#D61B6A',
+          800: '#B21758',
+          900: '#8E1246',
+          950: '#750F3A'
+        },
+        'athens-gray': {
+          DEFAULT: '#F8F9FA',
+          50: '#FFFFFF',
+          100: '#FFFFFF',
+          200: '#FFFFFF',
+          300: '#FFFFFF',
+          400: '#F8F9FA',
+          500: '#D7DDE3',
+          600: '#B7C1CB',
+          700: '#96A5B4',
+          800: '#75899C',
+          900: '#5B6D7F',
+          950: '#4F5F6F'
+        },
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
